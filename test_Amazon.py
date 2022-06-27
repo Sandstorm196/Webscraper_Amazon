04fa8f440094e9d8382743555890052885ff6b0a
Mansur Can <mansurcan@Mansurs-MacBook-Air.local>
2022-06-27 13:21:05 +0100

    unittest

--- test_Amazon.py
+++ test_Amazon.py
@@ -5,18 +5,19 @@ import sys
 import os
 from selenium.common.exceptions import ElementNotInteractableException
 
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# sys.path.append(os.path.pardir(os.path.pardir(__file__)))
 
-class TestDemo(unittest.TestCase):
-    
+sys.path.append("./Users/mansurcan/Documents/Ai_Core/Webscraper_Amazon/test_Amazon.py")
+
+class Test(unittest.TestCase):
     '''
     Initialize the scenario for the public methods:
     - accept_cookies()
     - search()
     - click_on_brand()
+    (Arrange, Act, Assert)
     '''
-    
-    def test_accept_cookies():
+    def test_accept_cookies(self):
         '''Test the accept_cookies button whether it is clicked on.'''
         
         Amazon.accept_cookies("//input[@type='submit' and @id='sp-cc-accept']")
@@ -27,7 +28,7 @@ class TestDemo(unittest.TestCase):
     #     '''Test the search term is on the website.'''
         
     #     expected_term = ['iPhone 13']
-    #     actual_term = self.search("//input[@id='twotabsearchtextbox']")
+    #     actual_term = Amazon.search("//input[@id='twotabsearchtextbox']")
     #     self.assertEqual(expected_term, actual_term)
         
     # def test_click_on_brand(self):
@@ -35,15 +36,21 @@ class TestDemo(unittest.TestCase):
     #     pass
         
         
-    def tearDown(self):
+    # def tearDown(self):
         
-        '''This is used to remove any of the variable set up from memory.'''
-        del self.Amazon
-    
+    #     '''This is used to remove any of the variable set up from memory.'''
+    #     self.driver.close()    
         
+# if __name__ == '__main__':
+#     '''Main method'''
+    
+#     unittest.main()
+    
+    # suite = unittest.defaultTestLoader.loadTestsFromTestCase(Test)
+    # unittest.TextTestRunner().run(suite)
     
     
-if __name__ == '__main__':
-    '''Main method'''
+    # unittest.main(argv=[''], verbosity=2, exit=False)
     
-    unittest.main(argv=[''], verbosity=2, exit=False)
+     
+  
